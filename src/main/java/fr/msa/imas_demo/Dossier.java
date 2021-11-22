b667f5c4ead8867fbee7136ba2f768c370646b53
pamAdmin <>
2021-11-22 13:16:18 +0000

    {/src/main/java/fr/msa/imas_demo/Dossier.java}

--- src/main/java/fr/msa/imas_demo/Dossier.java
+++ src/main/java/fr/msa/imas_demo/Dossier.java
@@ -6,12 +6,33 @@ package fr.msa.imas_demo;
 
 public class Dossier implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Dossier() {
-    }
+	private java.lang.String numsecu;
+	private java.lang.String typeDemarrage;
 
+	public Dossier() {
+	}
 
+	public java.lang.String getNumsecu() {
+		return this.numsecu;
+	}
 
+	public void setNumsecu(java.lang.String numsecu) {
+		this.numsecu = numsecu;
+	}
+
+	public java.lang.String getTypeDemarrage() {
+		return this.typeDemarrage;
+	}
+
+	public void setTypeDemarrage(java.lang.String typeDemarrage) {
+		this.typeDemarrage = typeDemarrage;
+	}
+
+	public Dossier(java.lang.String numsecu, java.lang.String typeDemarrage) {
+		this.numsecu = numsecu;
+		this.typeDemarrage = typeDemarrage;
+	}
 
 }
